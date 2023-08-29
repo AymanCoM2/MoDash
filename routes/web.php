94982eb9f9cfc7a56e91c4885da387376b04a2fd
= <aymancoom3@gmail.com>
2023-08-29 10:10:07 +0300

    Auth Scaffold

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,7 @@ Route::get('/page', function () {
     $results = DB::select('SELECT * FROM TM.areahierarchy');
     return view('page', compact('results'));
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
